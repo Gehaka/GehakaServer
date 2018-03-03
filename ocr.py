c76e1d59964544bdc3cb32766307b1fefcc4b786
George Lydakis <lydakis.george@gmail.com>
2018-03-03 10:38:20 -0800

    Updated process api

--- ocr.py
+++ ocr.py
@@ -1,79 +1,77 @@
 import time 
 import requests
-import cv2
-import operator
-import numpy as np
+
 
 _url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/RecognizeText'
 _key = '3197c93f163c49e2a07e2a1c7e7ac5f4'
 _maxNumRetries = 10
 
-def processRequest(json, data, headers, params):
-  """
-  Helper function to process the request to Project Oxford
+def processRequest( json, data, headers, params ):
 
-  Parameters:
-  json: Used when processing images from its URL. See API Documentation
-  data: Used when processing image read from disk. See API Documentation
-  headers: Used to pass the key information and the data type request
-  """
+    """
+    Helper function to process the request to Project Oxford
 
-  retries = 0
-  result = None
+    Parameters:
+    json: Used when processing images from its URL. See API Documentation
+    data: Used when processing image read from disk. See API Documentation
+    headers: Used to pass the key information and the data type request
+    """
 
-  while True:
-    response = requests.request(
-      'post', _url, json = json, data = data, headers = headers, params = params)
+    retries = 0
+    result = None
 
-    if response.status_code == 429:
-      print( "Message: %s" % ( response.json() ) )
-      if retries <= _maxNumRetries: 
-        time.sleep(1) 
-        retries += 1
-        continue
-      else: 
-        print( 'Error: failed after retrying!' )
+    while True:
+        response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
+
+        if response.status_code == 429:
+            print( "Message: %s" % ( response.json() ) )
+            if retries <= _maxNumRetries: 
+                time.sleep(1) 
+                retries += 1
+                continue
+            else: 
+                print( 'Error: failed after retrying!' )
+                break
+        elif response.status_code == 202:
+            result = response.headers['Operation-Location']
+        else:
+            print( "Error code: %d" % ( response.status_code ) )
+            print( "Message: %s" % ( response.json() ) )
         break
-    elif response.status_code == 202:
-      result = response.headers['Operation-Location']
-    else:
-      print( "Error code: %d" % ( response.status_code ) )
-      print( "Message: %s" % ( response.json() ) )
-    break
+        
+    return result
 
-  return result
+def getOCRTextResult( operationLocation, headers ):
+    """
+    Helper function to get text result from operation location
 
-def getOCRTextResult(operationLocation, headers):
-  """
-  Helper function to get text result from operation location
+    Parameters:
+    operationLocation: operationLocation to get text result, See API Documentation
+    headers: Used to pass the key information
+    """
 
-  Parameters:
-  operationLocation: operationLocation to get text result, See API Documentation
-  headers: Used to pass the key information
-  """
+    retries = 0
+    result = None
 
-  retries = 0
-  result = None
-
-  while True:
-    response = requests.request('get', operationLocation, json=None, data=None, headers=headers, params=None)
-    if response.status_code == 429:
-      print("Message: %s" % (response.json()))
-      if retries <= _maxNumRetries:
-        time.sleep(1)
-        retries += 1
-        continue
-      else:
-        print('Error: failed after retrying!')
+    while True:
+        response = requests.request('get', operationLocation, json=None, data=None, headers=headers, params=None)
+        if response.status_code == 429:
+            print("Message: %s" % (response.json()))
+            if retries <= _maxNumRetries:
+                time.sleep(1)
+                retries += 1
+                continue
+            else:
+                print('Error: failed after retrying!')
+                break
+        elif response.status_code == 200:
+            result = response.json()
+        else:
+            print("Error code: %d" % (response.status_code))
+            print("Message: %s" % (response.json()))
         break
-    elif response.status_code == 200:
-      result = response.json()
-    else:
-      print("Error code: %d" % (response.status_code))
-      print("Message: %s" % (response.json()))
-    break
 
-  return result
+    return result
 
 
 def process_image(urlImage):
@@ -88,7 +86,7 @@ def process_image(urlImage):
 
   result = None
   operationLocation = processRequest(json, data, headers, params)
-  if operationLocation != None:
+  if (operationLocation != None):
     headers = {}
     headers['Ocp-Apim-Subscription-Key'] = _key
     while True:
@@ -98,15 +96,14 @@ def process_image(urlImage):
         break
 
   if result is not None and result['status'] == 'Succeeded':
-    import pdb; pdb.set_trace()
-    print result
+    return parse_result(result)
 
 def parse_result(result):
-  pass
-
+  for res in result['recognitionResult']['lines']:
+    yield res['text']
 
 if __name__ == '__main__':
-  process_image('https://s3.us-east-2.amazonaws.com/hacktech2018/IMG_1043.jpg')
+  res = process_image('https://s3.us-east-2.amazonaws.com/hacktech2018/IMG_1043.jpg')
 
 
 
--- server.py
+++ server.py
@@ -1,12 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from ocr import process_image
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'hello world'
 
-@app.route('/process_image')
-def process_image():
-  image_url = request.args.get('image', None, type=str)
+@app.route('/process')
+def process():
+  image_url = request.args.get('image')
+  res = process_image(image_url)
+  return jsonify(list(res))
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
