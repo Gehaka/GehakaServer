7fe166d9a9653dad8bdcf7248288ab601aba5c54
George Lydakis <lydakis.george@gmail.com>
2018-03-03 18:10:43 -0800

    Error correction

--- errorCorrection.py
+++ errorCorrection.py
@@ -2,37 +2,37 @@ import keyword
 import re
 
 def errorCorrection(lines):
-    keyWordList = keyword.kwlist
-    operators = ['=','==','+','-','*','/','%','**','//','!=','<>','>','<','>=','<=']
-    for i in range(len(lines)):
-        lines[i] = re.sub(' +',' ',lines[i])
-        #Assignment Operator
-        if("=" in lines[i]):
-            continue
-        else:
-            temp = lines[i].replace(" ","")
-            #def function
-            if(temp[0:3] == "def"):
-                lines[i] = lines[i][0:3]+" "+temp[3:]
-                if(!lines[i][-3:-1] == "()"):
-                    lines[i] = lines[i] + "()"
-                if(!lines[i][-1:] == ":"):
-                    lines[i] = lines[i] + ":"
+  keyWordList = keyword.kwlist
+  operators = ['=','==','+','-','*','/','%','**','//','!=','<>','>','<','>=','<=']
+  for i in range(len(lines)):
+    lines[i] = re.sub(' +',' ',lines[i])
+    #Assignment Operator
+    if("=" in lines[i]):
+      continue
+    else:
+      temp = lines[i].replace(" ","")
+      #def function
+      if(temp[0:3] == "def"):
+        lines[i] = lines[i][0:3]+" "+temp[3:]
+        if(lines[i][-3:-1] != "()"):
+          lines[i] = lines[i] + "()"
+        if(lines[i][-1:] != ":"):
+          lines[i] = lines[i] + ":"
 
-            #Print Statement
-            if("\"" not in lines[i]):
-                continue
-            if(temp[0:5] != "print"):
-                temp = temp[0:5].replace(temp[0:5],"print")
-            if(temp[len(temp)-1] == "\"" and temp[5] != "\""):
-                temp = temp[:5]+"\""+temp[5:]
-            if(temp[len(temp)-1] != "\"" and temp[5] == "\""):
-                temp = temp+"\""
-            lines[i] = temp
-    return lines
+      #Print Statement
+      if("\"" not in lines[i]):
+        continue
+      if(temp[0:5] != "print"):
+        temp = temp[0:5].replace(temp[0:5],"print")
+      if(temp[len(temp)-1] == "\"" and temp[5] != "\""):
+        temp = temp[:5]+"\""+temp[5:]
+      if(temp[len(temp)-1] != "\"" and temp[5] == "\""):
+        temp = temp+"\""
+      lines[i] = temp
+  return lines
 
 def main():
-    res = errorCorrection(lines)
+  res = errorCorrection(lines)
 
 if __name__=="main":
-    main()
+  main()
