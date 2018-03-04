d15a24b06613ac5723f40b7b67edaffdc6125dc5
Code Begineer <hrandhaw@usc.edu>
2018-03-03 17:58:11 -0800

    Update errorCorrection.py

--- errorCorrection.py
+++ errorCorrection.py
@@ -10,10 +10,18 @@ def errorCorrection(lines):
         if("=" in lines[i]):
             lines[i] = lines[i]
         else:
+            temp = lines[i].replace(" ","")
+            #def function
+            if(temp[0:3] == "def"):
+                lines[i] = lines[i][0:3]+" "+temp[3:]
+                if(!lines[i][-3:-1] == "()"):
+                    lines[i] = lines[i] + "()"
+                if(!lines[i][-1:] == ":"):
+                    lines[i] = lines[i] + ":"
+
             #Print Statement
             if("\"" not in lines[i]):
                 continue
-            temp = lines[i].replace(" ","")
             if(temp[0:5] != "print"):
                 temp = temp[0:5].replace(temp[0:5],"print")
             if(temp[len(temp)-1] == "\"" and temp[5] != "\""):
