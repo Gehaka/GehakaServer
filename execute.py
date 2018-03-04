d3a113c685f63461fc2c3a8b141a10c7e90f9852
George Lydakis <lydakis.george@gmail.com>
2018-03-04 01:01:15 -0800

    bug fix

--- execute.py
+++ execute.py
@@ -4,8 +4,15 @@ def write_file(code):
   with open('output.py', 'w') as fp:
     fp.writelines(line + '\n' for line in code)
 
+def write_file_str(code):
+  with open('output.py', 'w') as fp:
+    fp.write(code)
+
 def execute(code):
-  write_file(code)
+  if isinstance(code, str):
+    write_file_str(code)
+  else:
+    write_file(code)
   try:
     out = subprocess.check_output(['python', 'output.py'], stderr=subprocess.STDOUT)
   except subprocess.CalledProcessError as e:
