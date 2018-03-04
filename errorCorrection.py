01ad2dde585427f3ed4324a776441199b55495ec
Code Begineer <hrandhaw@usc.edu>
2018-03-03 16:50:55 -0800

    Update errorCorrection.py

--- errorCorrection.py
+++ errorCorrection.py
@@ -1,1 +1,30 @@
-errorCorrection.py
+import keyword
+import re
+
+def errorCorrection(lines):
+    keyWordList = keyword.kwlist
+    operators = ['=','==','+','-','*','/','%','**','//','!=','<>','>','<','>=','<=']
+    for i in range(len(lines)):
+        lines[i] = re.sub(' +',' ',lines[i])
+        #Assignment Operator
+        if("=" in lines[i]):
+            lines[i] = lines[i]
+        else:
+            #Print Statement
+            if("\"" not in lines[i]):
+                continue
+            temp = lines[i].replace(" ","")
+            if(temp[0:5] != "print"):
+                temp = temp[0:5].replace(temp[0:5],"print")
+            if(temp[len(temp)-1] == "\"" and temp[5] != "\""):
+                temp = temp[:5]+"\""+temp[5:]
+            if(temp[len(temp)-1] != "\"" and temp[5] == "\""):
+                temp = temp+"\""
+            lines[i] = temp
+    return lines
+
+def main():
+    res = errorCorrection(lines)
+
+if __name__=="main":
+    main()
